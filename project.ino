f6b863fb0b32b1dec21924df4afb6d88f59ad378
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-03 22:53:03 +0200

    Update project.ino

--- project.ino
+++ project.ino
@@ -1,7 +1,8 @@
-// Dummy Data
-char Total_Dis[] = "107";
+// Dummy Data 
 char Total_Time[] = "389";
 char Current_velocity[] = "5";
+char Total_Dis[] = "107";
+
 char Angle[] = "258" ;
 
 void main(){
